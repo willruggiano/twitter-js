3a08b9a762d64bf74e2ff15847aeb07f300b4af5
Will Ruggiano <will@ruggianofamily.com>
2015-06-17 13:08:03 -0400

    added tweetBank.js

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 var express = require('express')
 var morgan = require('morgan')
+var swig = require('swig')
+var tweetBank = require('tweetBank')
+
 var app = express()
+
+swig.setDefaults({cache:false})
 
 app.use(morgan('dev'))
 
+app.engine('html', swig.renderFile)
+app.set('view engine', 'html')
+app.set('views', './views')
+
+var people = [{name:'Full'}, {name:'Stacker'}, {name:'Son'}]
+
 app.get('/', function(req, res){
-  res.send('Welcome to twitter!')
+  res.render('index', {title:'Hall of Fame', people: people})
 })
-
-app.get('/news', function(req, res){
-  res.send('There might be some news')
-})
-
-
-
 
 app.listen(3000, function(){
   console.log('server listening')
